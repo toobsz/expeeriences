dea7b33e6e145505ec312469eec94a2c0e4943b0
Hunter Miller <millerhs@vcu.edu>
2017-09-16 23:36:52 -0400

    Added a little more stuff

--- .NET/Expeeriences.Web/DynamoDB/DynamoConnection.cs
+++ .NET/Expeeriences.Web/DynamoDB/DynamoConnection.cs
@@ -1,24 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
-using System.Runtime.InteropServices.ComTypes;
 using Amazon;
 using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
 
 namespace DynamoDB
 {
     public class DynamoConnection
     {
+        private IAmazonDynamoDB _dynamoDb;
+        private const string TableName = "TestTable";
+        private const string AccessKey = "AKIAIIUVKT6TXYWRUEXA";
+        private const string SecretKey = "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";
+
+        public DynamoConnection(IAmazonDynamoDB dynamoDb)
+        {
+            _dynamoDb = dynamoDb;
+
+            _dynamoDb.CreateTableAsync(new CreateTableRequest(TableName, new List<KeySchemaElement>
+            {
+                new KeySchemaElement { AttributeName = "Row1" },
+                new KeySchemaElement { AttributeName = "Row2" }
+            }));
+        }
+
         public void StartDynamo()
         {
-            var dynamoConfig = new AmazonDynamoDBConfig();
-            dynamoConfig.RegionEndpoint = RegionEndpoint.USEast1;
-            dynamoConfig.UseHttp = true;
+            var dynamoCredentials = new BasicAWSCredentials(AccessKey, SecretKey);
+            var dynamoConfig = new AmazonDynamoDBConfig { RegionEndpoint = RegionEndpoint.USEast1};
 
             AmazonDynamoDBClient dynamoClient;
-
             try
             {
-                dynamoClient = new AmazonDynamoDBClient();
+                dynamoClient = new AmazonDynamoDBClient(dynamoCredentials, dynamoConfig);
             }
             catch (Exception e)
             {
